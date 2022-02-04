c3b764dea15bdb810bead5b8cb18ddbf6d737eef
frontendprof <abdumalksharipov@gmail.com>
2022-02-04 06:28:55 +0500

    connectiong to mongodb

---
+++ pages/api/rooms/index.js
@@ -0,0 +1,11 @@
+import nc from 'next-connect';
+import dbConnect from '../../../config/dbConnect';
+import { allRooms, newRoom } from '../../../controllers/roomController';
+
+const handler = nc();
+dbConnect();
+
+handler.get(allRooms);
+handler.post(newRoom);
+
+export default handler;
